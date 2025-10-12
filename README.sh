caaebbdd3eb5f966793561d2880f679d3253518a
Duyal Yolcu <94126637+qudent@users.noreply.github.com>
2025-10-13 00:25:25 +0200

    README.md -> README.sh

---
+++ README.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+# Functions for parallel worktree operations
+# This adds a new worktree for the given branch, cd's into it, and runs pnpm install if package.json is present
+# pnpm install is better than npm because it uses the pnpm store and hard links to save space and time
+add_worktree() {
+  local NEWBRANCH="$1"
+  local NEWWORKTREE="$(git rev-parse --show-toplevel).worktrees/$NEWBRANCH"
+  git worktree add "$NEWWORKTREE"
+  cd "$NEWWORKTREE"
+  if [[ -f package.json ]]; then
+    pnpm install || echo "pnpm failed"
+  else
+    echo "No package.json found, skipping pnpm install"
+  fi
+}
+
+# This merges the current state of the branch in the current worktree into the "parent worktree" and goes back to the working branch
+merge_to_parent() {
+  local BRANCHTOMERGE="$(git rev-parse --abbrev-ref HEAD)"
+  local CURRENT_PATH="$(pwd)"
+  cd "${CURRENT_PATH%%.worktrees/*}"
+  git merge "$BRANCHTOMERGE"
+  cd "$CURRENT_PATH"
+}
+
+# this "aborts" the branch: deletes branch and worktree, and ends up in the "parent" worktree. git worktree remove --force and git branch -D to force delete even with unmerged commits
+abort_branch() {
+  local BRANCHTOMERGE="$(git rev-parse --abbrev-ref HEAD)"
+  local CURRENT_PATH="$(pwd)"
+  cd "${CURRENT_PATH%%.worktrees/*}"
+  git worktree remove --force "$CURRENT_PATH"
+  git branch -D "$BRANCHTOMERGE"
+}
